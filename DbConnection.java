c5598cffc1de9702f9e7057ca8a701e1973864cf
Khrunch <keionclei@gmail.com>
2025-11-19 21:46:05 +0800

    final fix to sql scripts

--- DbConnection.java
+++ DbConnection.java
@@ -65,7 +65,7 @@ public class DbConnection {
     Email VARCHAR(100), 
     Address VARCHAR(250), 
     Password VARCHAR(50),
-    PRIMARY KEY (AccountID))
+    PRIMARY KEY (AccountID));
     """;
 
     String CreateTableStaff =
@@ -79,19 +79,18 @@ public class DbConnection {
     Availability VARCHAR(50), 
     Password VARCHAR(50),
     PRIMARY KEY (StaffID),
-    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID))
+    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID));
     """;
 
     String CreateTableService =
     """
-    DROP TABLE IF EXISTS Service;
-    CREATE TABLE Service (
-    ServiceID INT AUTO_INCREMENT, 
+    CREATE TABLE IF NOT EXISTS Service (
+    ServiceID INT NOT NULL, 
     ServiceName VARCHAR(100), 
     ServiceType VARCHAR(50), 
     DepartmentID INT,
     PRIMARY KEY (ServiceID),
-    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID))
+    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID));
     """;
 
     String CreateTableServiceRequest =
@@ -109,16 +108,15 @@ public class DbConnection {
     PRIMARY KEY (RequestID),
     FOREIGN KEY (AccountID) REFERENCES Citizen(AccountID),
     FOREIGN KEY (StaffID) REFERENCES Staff(StaffID),
-    FOREIGN KEY (ServiceID) REFERENCES Service(ServiceID))
+    FOREIGN KEY (ServiceID) REFERENCES Service(ServiceID));
     """;
 
     String CreateTableDepartment =
     """
-    DROP TABLE IF EXISTS Department;
-    CREATE TABLE Department (
+    CREATE TABLE IF NOT EXISTS Department (
     DepartmentID INT NOT NULL, 
     DepartmentName VARCHAR(100),
-    PRIMARY KEY (DepartmentID))
+    PRIMARY KEY (DepartmentID));
     """;
 
     String CreateTableAssignmentResolution = """
@@ -131,7 +129,7 @@ public class DbConnection {
     StatusAfterAction VARCHAR(20),
     PRIMARY KEY (ResolutionID),
     FOREIGN KEY (RequestID) REFERENCES ServiceRequest(RequestID),
-    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID))
+    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID));
     """;
 
     String CreateTableReopenRequest = """
@@ -143,13 +141,13 @@ public class DbConnection {
     ReopenDate DATE NOT NULL,
     PRIMARY KEY (ReopenID),
     FOREIGN KEY (RequestID) REFERENCES ServiceRequest(RequestID),
-    FOREIGN KEY (CitizenID) REFERENCES Citizen(AccountID))
+    FOREIGN KEY (CitizenID) REFERENCES Citizen(AccountID));
     """;
 
     //Scripts that insert data into tables
 
 String InsertDepartments = """
-    INSERT INTO Department (DepartmentID, DepartmentName) VALUES
+    INSERT INGORE INTO Department (DepartmentID, DepartmentName) VALUES
     (1, 'Department of Engineering and Public Works'),
     (2, 'City General Services Office'),
     (3, 'Department of Public Services'),
@@ -158,39 +156,39 @@ String InsertDepartments = """
     """;
 
 String InsertServicesDept1 = """
-    INSERT INTO Service (ServiceName, ServiceType, DepartmentID) VALUES
-    ('Road Damage Inspection Request', 'Inspection', 1),
-    ('Drainage or Sewerage Cleaning Request', 'Maintenance', 1),
-    ('Street Signage Issue Complaint', 'Complaint', 1),
-    ('Infrastructure Damage Assessment', 'Inspection', 1);
+    INSERT IGNORE INTO Service (ServiceID, ServiceName, ServiceType, DepartmentID) VALUES
+    (1, 'Road Damage Inspection Request', 'Inspection', 1),
+    (2, 'Drainage or Sewerage Cleaning Request', 'Maintenance', 1),
+    (3, 'Street Signage Issue Complaint', 'Complaint', 1),
+    (4, 'Infrastructure Damage Assessment', 'Inspection', 1);
     """;
 
 String InsertServicesDept2 = """
-    INSERT INTO Service (ServiceName, ServiceType, DepartmentID) VALUES
-    ('City Furniture Repair Request', 'Repair', 2),
-    ('Hazardous Waste Disposal Request', 'Cleaning', 2),
-    ('City Building Maintenance Request', 'Maintenance', 2);
+    INSERT INTO Service (ServiceID, ServiceName, ServiceType, DepartmentID) VALUES
+    (5, 'City Furniture Repair Request', 'Repair', 2),
+    (6, 'Hazardous Waste Disposal Request', 'Cleaning', 2),
+    (7, 'City Building Maintenance Request', 'Maintenance', 2);
     """;
 
 String InsertServicesDept3 = """
-    INSERT INTO Service (ServiceName, ServiceType, DepartmentID) VALUES
-    ('Garbage Collection Request', 'Cleaning', 3),
-    ('Illegal Dumping Complaint', 'Complaint', 3),
-    ('Street Sweeping Request', 'Cleaning', 3),
-    ('Public Trash Bin Maintenance Request', 'Maintenance', 3);
+    INSERT INTO Service (ServiceID, ServiceName, ServiceType, DepartmentID) VALUES
+    (8, 'Garbage Collection Request', 'Cleaning', 3),
+    (9, 'Illegal Dumping Complaint', 'Complaint', 3),
+    (10, 'Street Sweeping Request', 'Cleaning', 3),
+    (11, 'Public Trash Bin Maintenance Request', 'Maintenance', 3);
     """;
 
 String InsertServicesDept4 = """
-    INSERT INTO Service (ServiceName, ServiceType, DepartmentID) VALUES
-    ('Playground Equipment Maintenance Request', 'Maintenance', 4),
-    ('Recreational Program Registration Inquiry', 'Information', 4);
+    INSERT INTO Service (ServiceID, ServiceName, ServiceType, DepartmentID) VALUES
+    (12, 'Playground Equipment Maintenance Request', 'Maintenance', 4),
+    (13, 'Recreational Program Registration Inquiry', 'Information', 4);
     """;
 
 String InsertServicesDept5 = """
-    INSERT INTO Service (ServiceName, ServiceType, DepartmentID) VALUES
-    ('Park Grounds Maintenance Request', 'Maintenance', 5),
-    ('Tree Trimming or Pruning Request', 'Maintenance', 5),
-    ('Park Facility Damage Inspection', 'Inspection', 5);
+    INSERT INTO Service (ServiceID, ServiceName, ServiceType, DepartmentID) VALUES
+    (14, 'Park Grounds Maintenance Request', 'Maintenance', 5),
+    (15, 'Tree Trimming or Pruning Request', 'Maintenance', 5),
+    (16, 'Park Facility Damage Inspection', 'Inspection', 5);
     """;
 
 
